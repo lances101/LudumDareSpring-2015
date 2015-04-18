a6a89956e55ea6d97d2602c460161be1881515be
Adrian Perez Marrero <apematica@gmail.com>
2015-04-18 17:26:32 +0100

    Pachinko

---
+++ Ludum Dare 2015 Spring/Assets/Workflow/Adrian/Scripts/HoleController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleController : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<BallController>())
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Ludum Dare 2015 Spring/Assets/Workflow/Adrian/Scripts/PachinkoController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PachinkoController : MonoBehaviour {
+    public GameObject ring;
+    public GameObject ball;
+
+    void Start()
+    {
+        ring.GetComponent<Rigidbody2D>().velocity = Vector2.right * 2;
+    }
+/**
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(ball, ring.transform.position, Quaternion.identity);
+        }
+    }
+    **/
+    public void CreateBall()
+    {
+        Instantiate(ball,ring.transform.position,Quaternion.identity);
+    }
+
+}
